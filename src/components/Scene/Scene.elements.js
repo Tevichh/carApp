2dd8971f904428ce92714b92c93d452d785429f4
Tevichh <83615857+Tevichh@users.noreply.github.com>
2024-01-30 20:30:08 -0500

    sedan cel 4

--- src/components/Scene/Scene.elements.js
+++ src/components/Scene/Scene.elements.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ContainerScene = styled.div`
   width: 100%;
   height: 100vh;
-  margin: auto;
+  margin: 0;
   -webkit-tap-highlight-color: transparent;
   tap-highlight-color: transparent;
 
